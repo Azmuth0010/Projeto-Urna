b8d2abe8842b667686ce7b92ca14395e30816b7d
= <=>
2024-10-24 20:39:02 -0300

    Adicionando projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExternoComponent } from "./externo/externo.component";
+import { InternoComponent } from "./interno/interno.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ExternoComponent, InternoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
